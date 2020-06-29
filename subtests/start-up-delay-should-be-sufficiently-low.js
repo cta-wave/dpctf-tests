ae0da67a85002ebdbb42cdf8084927fed2055bf6
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2020-06-29 12:43:32 +0200

    Implemented subtest 3 The start-up delay should be sufficiently low

---
+++ subtests/start-up-delay-should-be-sufficiently-low.js
@@ -0,0 +1,28 @@
+function startUpDelaySmallEnough(tsMax) {
+  var playingEventCounter = 0;
+  var playbackInitiated = 0.0;
+  var presentationTimeOfFirstSample = 0.0;
+  var t = async_test(
+    "The start-up delay should be sufficiently low (120ms max)."
+  );
+
+  player.addEventListener("onSegmentLoaded", function (event) {
+    if (event.totalSegmentsLoaded === 2) {
+      player.play();
+      playbackInitiated = new Date().getTime();
+    }
+  });
+
+  video.addEventListener("playing", function () {
+    if (playingEventCounter === 0) {
+      presentationTimeOfFirstSample = new Date().getTime();
+      t.step(function () {
+        delay = presentationTimeOfFirstSample - playbackInitiated;
+        assert_less_than(delay, tsMax, "The delay is " + delay + " ms.");
+        t.done();
+      });
+      playingEventCounter++;
+    }
+  });
+}
+
