3c2d134547b918a20df3ef8dd1386d5720663dbb
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2020-07-31 10:59:48 +0200

    Implemented main test 8.3 Random Access to Fragment

--- subtests/playback-duration-matches-cmaf-track-duration.js
+++ subtests/playback-duration-matches-cmaf-track-duration.js
@@ -7,12 +7,12 @@ function playbackDurationMatchesCMAFTrackDuration() {
   });
 
   expectedResult = videosegments
-    .map(function (segment) {
-      return segment.duration;
-    })
-    .reduce(function (sum, value) {
-      return sum + value;
-    });
+      .map(function (segment) {
+        return segment.duration;
+      })
+      .reduce(function (sum, value) {
+        return sum + value;
+      });
 
   //add first segment (number:0)
   sumTime += player.getPlayingSegment().duration;
@@ -23,20 +23,20 @@ function playbackDurationMatchesCMAFTrackDuration() {
 
   async_test(function (test) {
     player.video.addEventListener(
-      "ended",
-      test.step_func(function () {
-        assert_equals(
-          sumTime,
-          expectedResult,
-          "Sum of duration matches playback duration"
-        );
-        assert_equals(
-          player.video.currentTime,
-          expectedResult,
-          "Current time matches playback duration"
-        );
-        test.done();
-      })
+        "ended",
+        test.step_func(function () {
+          assert_equals(
+              sumTime,
+              expectedResult,
+              "Sum of duration matches playback duration"
+          );
+          assert_equals(
+              player.video.currentTime,
+              expectedResult,
+              "Current time matches playback duration"
+          );
+          test.done();
+        })
     );
   }, "Playback-Duration matches CMAF-Track-Duration");
 }
@@ -56,18 +56,18 @@ function playbackDurationMatchesCMAFTrackDurationIncludingRandomAccessTime() {
       return player.videoSegments[key];
     });
     cmafTackLength = videoSegments
-      .map(function (segment) {
-        return segment.duration;
-      })
-      .reduce(function (sum, value) {
-        return sum + value;
-      });
+        .map(function (segment) {
+          return segment.duration;
+        })
+        .reduce(function (sum, value) {
+          return sum + value;
+        });
 
     assert_between_exclusive(
-      randomAccessTime,
-      0,
-      cmafTackLength,
-      "random access time is in the range of video duration"
+        randomAccessTime,
+        0,
+        cmafTackLength,
+        "random access time is in the range of video duration"
     );
 
     if (randomAccessTime > cmafTackLength || randomAccessTime < 0) {
@@ -86,39 +86,37 @@ function playbackDurationMatchesCMAFTrackDurationIncludingRandomAccessTime() {
 
     var result;
     player.addEventListener(
-      "onPlayingSegmentChange",
-      test.step_func(function () {
-        if (!startSegment) return;
-        sumTime += player.getPlayingSegment().duration;
-      })
+        "onPlayingSegmentChange",
+        test.step_func(function () {
+          if (!startSegment) return;
+          sumTime += player.getPlayingSegment().duration;
+        })
     );
 
     player.video.addEventListener(
-      "ended",
-      test.step_func(function () {
-        assert_equals(
-          sumTime + remainingTimeInSegment,
-          expectedResult,
-          "playing duration equals CMAF Track duration."
-        );
-        assert_equals(
-          player.video.currentTime,
-          cmafTackLength,
-          "current time equals CMAF Track duration."
-        );
-        test.done();
-      })
+        "ended",
+        test.step_func(function () {
+          assert_equals(
+              sumTime + remainingTimeInSegment,
+              expectedResult,
+              "playing duration equals CMAF Track duration."
+          );
+          assert_equals(
+              player.video.currentTime,
+              cmafTackLength,
+              "current time equals CMAF Track duration."
+          );
+          test.done();
+        })
     );
   }, "Playback-Duration matches CMAF-Track-Duration by random access time");
 }
 
 function playbackDurationMatchesCMAFTrackDurationIncludingRandomAccessFragment(
-  randomAccessFragmentIndex
+    randomAccessFragmentIndex
 ) {
   var sumTime = 0;
   var videosegments = [];
-  var earliestStart = 0;
-  var cmafTackLength = 0;
   var expectedResult = 0;
   var randomAccessFragment = null;
 
@@ -126,52 +124,46 @@ function playbackDurationMatchesCMAFTrackDurationIncludingRandomAccessFragment(
     videosegments = Object.keys(player.videoSegments).map(function (key) {
       return player.videoSegments[key];
     });
-    randomAccessFragment = videosegments[randomAccessFragmentIndex];
-    var inArray = false;
+    randomAccessFragment = player.getPlayingSegment();
 
-    for (var i = 0; i < videosegments.length; i++) {
-      if (isEqual(randomAccessFragment, videosegments[i])) {
-        inArray = true;
-        earliestStart = cmafTackLength;
-      }
-      cmafTackLength += videosegments[i].duration;
-    }
-    if (!inArray) {
-      assert_true(false, "randomAccessFragment is included");
-      test.done();
-    }
+    var cmafTackLength = videosegments
+        .map(function (segment) {
+          return segment.duration;
+        })
+        .reduce(function (sum, value) {
+          return sum + value;
+        });
 
-    expectedResult = cmafTackLength - earliestStart;
-    player.video.currentTime = earliestStart;
-    player.handleTimeUpdate();
+    expectedResult = cmafTackLength - player.video.currentTime;
+
     if (randomAccessFragmentIndex === 0)
       sumTime += player.getPlayingSegment().duration;
 
     player.addEventListener(
-      "onPlayingSegmentChange",
-      test.step_func(function (segment) {
-        if (isEqual(segment, randomAccessFragment)) return;
-        sumTime += player.getPlayingSegment().duration;
-      })
+        "onPlayingSegmentChange",
+        test.step_func(function (segment) {
+          if (isEqual(segment, randomAccessFragment)) return;
+          sumTime += player.getPlayingSegment().duration;
+        })
     );
 
     player.video.addEventListener(
-      "ended",
-      test.step_func(function () {
-        assert_equals(
-          sumTime,
-          expectedResult,
-          "Sum of fragment duration matches cmaf duration"
-        );
+        "ended",
+        test.step_func(function () {
+          assert_equals(
+              sumTime,
+              expectedResult,
+              "Sum of fragment duration matches cmaf duration"
+          );
 
-        assert_equals(
-          player.video.currentTime,
-          cmafTackLength,
-          "playback duration matches cmaf track duration"
-        );
+          assert_equals(
+              player.video.currentTime,
+              cmafTackLength,
+              "playback duration matches cmaf track duration"
+          );
 
-        test.done();
-      })
+          test.done();
+        })
     );
   }, "Playback-Duration matches CMAF-Track-Duration by random access fragment");
 }
